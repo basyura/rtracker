32f4ccbcd910a36e49e80c841e3c600a216e099f
basyura <basyura@gmail.com>
2021-11-21 10:41:44 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import path from 'path';
+import { BrowserWindow, app, session } from 'electron';
+import { searchDevtools } from 'electron-search-devtools';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+/// #if DEBUG
+const execPath =
+  process.platform === 'win32'
+    ? '../node_modules/electron/dist/electron.exe'
+    : '../node_modules/.bin/electron';
+
+if (isDev) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('electron-reload')(__dirname, {
+    electron: path.resolve(__dirname, execPath),
+    forceHardReset: true,
+    hardResetMethod: 'exit',
+  });
+}
+/// #endif
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({ width: 200, height: 300 });
+
+  if (isDev) mainWindow.webContents.openDevTools({ mode: 'detach' });
+  mainWindow.loadFile('dist/index.html');
+  mainWindow.setAlwaysOnTop(true);
+  mainWindow.setPosition(-300, 0);
+};
+
+app.whenReady().then(async () => {
+  if (isDev) {
+    const devtools = await searchDevtools('REACT');
+    if (devtools) {
+      await session.defaultSession.loadExtension(devtools, {
+        allowFileAccess: true,
+      });
+    }
+  }
+
+  createWindow();
+});
+
+app.once('window-all-closed', () => app.quit());
---
+++ src/web/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+export const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+};
